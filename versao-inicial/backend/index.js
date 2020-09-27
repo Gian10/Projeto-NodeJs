450c017a5e10c78d4a7eb7107c659667299b5836
Gian10 <gianaraujo@hotmail.com>
2020-09-26 22:59:07 -0300

    usando consign

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const consign = require('consign')
 
+// // gerenciar meu middlewares 
+consign()
+    //lendo os arquivos
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
+    
+app.post('/teste', (req, res)=>{
+    res.send(req.body)
+})
 
 app.listen(3000, ()=>{
     console.log("executando")
